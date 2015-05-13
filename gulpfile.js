d8ec982afaa3a5b40a0d14d4d4b11e39c2544037
Annabel Church <bells.stuff+gitcommitme@gmail.com>
2015-05-13 15:20:25 +1200

    Force standardised quotes in build

--- gulpfile.js
+++ gulpfile.js
@@ -40,7 +40,7 @@ gulp.task('html', ['styles'], function () {
     .pipe($.if('*.css', $.csso()))
     .pipe(assets.restore())
     .pipe($.useref())
-    .pipe($.if('*.html', $.minifyHtml({conditionals: true, loose: true})))
+    .pipe($.if('*.html', $.minifyHtml({conditionals: true, loose: true, quotes: true })))
     .pipe(gulp.dest('dist'));
 });
 
