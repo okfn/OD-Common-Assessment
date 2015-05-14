72d409e0d7683ce1d330c4d5efef2b5833f78dc5
Annabel Church <bells.stuff+gitcommitme@gmail.com>
2015-05-14 19:11:46 +1200

    moving js

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,11 +2,3 @@
 /* jshint devel:true */
 //console.log('\'Allo \'Allo!');
 
-function bindDropdownEvents() {
-    $('#filter-dropdown').click(function(e) {
-        console.log('stop');
-            e.stopPropagation();
-    });
-}
-
-
