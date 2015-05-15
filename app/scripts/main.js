9a0c5de11298a6a5964b62c7aab94d16be71d92e
Annabel Church <bells.stuff+gitcommitme@gmail.com>
2015-05-15 16:04:30 +1200

    Moving JS out to services, and app. Changing templates and js to new model.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,4 +1,43 @@
 'use strict';
 /* jshint devel:true */
-//console.log('\'Allo \'Allo!');
+
+var app = (function () {
+    function bindDropdownEvents() {
+          $('#filter-dropdown ul').click(function(e) {
+              console.log('stop');
+                  e.stopPropagation();
+          });
+      }
+    function bindToTemplate(element, template, incomingData) {
+        var bound = new Ractive({
+          el: element,
+          template: template,
+          data: incomingData
+        });
+        return bound;
+    }
+    function showInfo(data, tabletop) {
+        var placesData = tabletop.sheets("places").elements;
+        console.log(placesData);
+
+        var bindTable = bindToTemplate("#table", "#template-table", {"places": placesData });
+
+        //$("#fullTable").html(ich.countries(cc));
+        //  var bindFilter = new Ractive({
+        //   el: "#filter",
+        //   template: "#template-filter",
+        //   data: { items: filterData }
+        // });
+
+        //bindDropdownEvents();
+    }
+
+    function runApp(){
+        services.getData(showInfo);
+    }
+
+    return {
+        start: runApp
+    };
+}());
 
---
+++ app/scripts/services.js
@@ -0,0 +1,20 @@
+'use strict';
+/* jshint devel:true */
+
+var services = (function () {
+
+    function getDataFromGoogleSpreadsheet(callback) {
+      console.log(callback, "callback")
+        var URL = "1kOOvztwbY1RNm545RKG8Ua6bh2GgX-P_wFadPkdH2ig";
+        Tabletop.init( { key: URL, callback: callback, simpleSheet: false, prettyColumnNames: false } );
+
+        // $('#apply').on('click', function() {
+        //     $('#filter-dropdown').trigger('hide.bs.dropdown');
+        // });
+    }
+
+    return {
+        getData: getDataFromGoogleSpreadsheet
+    };
+}());
+
