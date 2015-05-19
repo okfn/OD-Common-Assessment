d1e4db74eb56a0311a7cda9ba56394789e708c72
Annabel Church <bells.stuff+gitcommitme@gmail.com>
2015-05-19 19:14:45 +1200

    Partial refactor of visible indicators

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -9,6 +9,7 @@ var app = (function() {
     });
   }
 
+// TODO: break out into modules
   function showInfo(data, tabletop) {
     // deconstruct data with tabletop
     var placesData = tabletop.sheets('places').elements;
@@ -20,25 +21,12 @@ var app = (function() {
     console.log(indicatorsData);
     //console.log(valuesData);
 
-    // Get visible indicators
-    var indicatorsForScoring = {};
-    var countInitialIndicators = 0
-    indicatorsData.filter(function(indicator) {
-      var visible = indicator.scoring === 'Y' && indicator.default === 'Y';
-      if (visible) {
-        countInitialIndicators += 1;
-      }
-      return visible;
-    }).map(function(indicator) {
-      return indicatorsForScoring[indicator.id] = true;
-    });
-
     // Map variable and indicator data into results
     var resultsData = placesData.map(function(place) {
       place.values = [];
       valuesData.filter(function(value) {
         if (value['placeid'] === place.id) {
-          value['indicatorVisible'] = indicatorsForScoring.hasOwnProperty(value['indicatorid']);
+         // value['indicatorVisible'] = indicatorsForScoring.hasOwnProperty(value['indicatorid']);
           value['normalised'] = parseInt(value['normalised']) || 0;
           place['values'].push(value);
         }
@@ -48,18 +36,31 @@ var app = (function() {
     });
 
     //  console.log(valuesData)
-    console.log(placesData);
+    //console.log(placesData);
     //console.log(valuesData);
 
-    // visible
-    //console.log(indicatorsForScoring);
-
+    // filter indicators to make them easier to handle, visibility, default etc
     var indicatorsFiltered = {};
+    var forScoring = {};
+    var count = 0;
+
     for (var i = 0; i < indicatorsData.length; i++) {
+      if(utilities.isTrue(indicatorsData[i]['scoring'])) {
+        // set initial state for observable scores
+        forScoring[indicatorsData[i]['id']] = {value: false};
+        if(utilities.isTrue(indicatorsData[i]['default'])) {
+          count+= 1;
+          forScoring[indicatorsData[i]['id']]['value'] = true;
+        }
+      }
       indicatorsFiltered[indicatorsData[i]['id']] = indicatorsData[i];
     };
 
-    //console.log(indicatorsFiltered);
+    var Component = Ractive.extend({
+      isolated: false,
+      template: '#template-filter'
+    });
+
     var ractive = new Ractive({
       el: '#table',
       template: '#template-table',
@@ -67,8 +68,8 @@ var app = (function() {
         places: placesData,
         indicators: indicatorsFiltered,
         scoring: {
-          scoringIndicators: indicatorsForScoring,
-          numberOfVisibleIndicators: countInitialIndicators,
+          scoringIndicators: forScoring,
+          visibleIndicators: count,
           score: function(values) {
             //console.log(values)
             var v = values.filter(function(value) {
@@ -90,32 +91,41 @@ var app = (function() {
         },
         getIndicator: function(indicatorId, value) {
           return this.get('indicators')[indicatorId][value];
+        },
+        isVisible: function(indicatorId) {
+          return this.get('scoring.scoringIndicators')[indicatorId]['value'];
         }
       },
       computed: {
 
-      }
+      },
+      components: { Component: Component }
     });
 
+    console.log(forScoring);
+    console.log(indicatorsFiltered);
+
+    // sorting observers
     ractive.on('sorting.sort', function(event, column) {
       this.set('sorting.column', column);
       this.set('sorting.direction', this.get('sorting.direction') * -1);
     });
 
-    ractive.observe('scoring.scoringIndicators.*', function(newValue, oldValue, keypath) {
-      console.log(newValue, oldValue, keypath);
+
+    // visible indicators
+    var observer = ractive.observe( 'scoring.scoringIndicators.*', function ( newValue, oldValue, keypath ) {
+     // console.log( keypath + ' changed to ' );
+      //console.log(newValue);
+      //console.log(oldValue)
     });
 
-    // bindTable.set('data.numberOfVisibleIndicators', countInitialIndicators);
-    //console.log(bindTable.get(numberOfVisibleIndicators))
-    //this.set( 'items[*].completed', event.node.checked );
-    //
-    //         //$("#fullTable").html(ich.countries(cc));
-    //  var bindFilter = new Ractive({
-    //   el: "#filter",
-    //   template: "#template-filter",
-    //   data: { items: filterData }
+
+    //ractive.set( 'scoring.scoringIndicators.odi-score', true )
+    // ractive.observe('scoring.scoringIndicators.*', function(newValue, oldValue, keypath) {
+    //   console.log(newValue, oldValue, keypath);
     // });
+
+    //this.set( 'items[*].completed', event.node.checked );
 
     //bindDropdownEvents();
   }
