b4c3405f4297a12e0466e7f24dfa8dc79a93e624
Annabel Church <bells.stuff+gitcommitme@gmail.com>
2015-05-20 20:59:26 +1200

    Extending reactive with remove

---
+++ app/scripts/ractive.extend.js
@@ -0,0 +1,9 @@
+Ractive.prototype.remove = function(keypath, item){
+    var array = this.get(keypath),
+          index = array.indexOf(item)
+    if(index!==-1) {
+         return this.splice(keypath, index, 1)
+    } else {
+         return Promise.defer()
+    }
+}
